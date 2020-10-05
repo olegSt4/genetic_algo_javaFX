ab6d3fae36d6f1bee4c8cabb5661d01afec54fdf
olegSt4 <stefura2010@ukr.net>
2020-10-06 01:49:21 +0300

    feat(DisplayWindowController): points displaying added

--- src/main/java/controllers/DisplayWindowController.java
+++ src/main/java/controllers/DisplayWindowController.java
@@ -5,6 +5,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import models.Point;
 
@@ -16,15 +18,30 @@ public class DisplayWindowController {
 
     public void start(Parent root, String startId, List<Point> points, String iter, String mutPercent) {
         Scene scene = new Scene(root);
-
-        Label test = new Label("From point " + startId);
-        test.setLayoutX(100);
-        test.setLayoutY(100);
-        anchorPane.getChildren().add(test);
+        showPoints(points, startId);
 
         Stage stage = new Stage();
         stage.setTitle("Genetic Algorithm");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void showPoints(List<Point> points, String startId) {
+        double activeWidth = anchorPane.getPrefWidth();
+        double activeHeight = anchorPane.getPrefHeight();
+
+        int id = 1;
+        for (Point point : points) {
+            Circle circle = new Circle(point.x * activeWidth, point.y * activeHeight, 5);
+            if (id == Integer.parseInt(startId)) {
+                circle.setRadius(7);
+                circle.setFill(Color.RED);
+            }
+            Label idText = new Label(String.valueOf(id++));
+            idText.setLayoutX(point.x * activeWidth + 5);
+            idText.setLayoutY(point.y * activeHeight);
+
+            anchorPane.getChildren().addAll(circle, idText);
+        }
     }
 }
