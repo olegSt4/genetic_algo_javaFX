c9f0f221ecadadfa90af6ec58c130200d4e64341
olegSt4 <stefura2010@ukr.net>
2020-10-06 01:37:00 +0300

    fix(DisplayWindowController): signature of start method changed, anchorPane variable added

--- src/main/java/controllers/DisplayWindowController.java
+++ src/main/java/controllers/DisplayWindowController.java
@@ -1,16 +1,26 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import models.Point;
 
 import java.util.List;
 
 public class DisplayWindowController {
+    @FXML
+    private AnchorPane anchorPane;
 
-    public void start(Parent root, List<Point> points, String iter, String mutPercent) {
+    public void start(Parent root, String startId, List<Point> points, String iter, String mutPercent) {
         Scene scene = new Scene(root);
+
+        Label test = new Label("From point " + startId);
+        test.setLayoutX(100);
+        test.setLayoutY(100);
+        anchorPane.getChildren().add(test);
 
         Stage stage = new Stage();
         stage.setTitle("Genetic Algorithm");
