31b1b10050fa8a5bc79d5664d75fd93884a56bc8
olegSt4 <stefura2010@ukr.net>
2020-10-05 23:46:32 +0300

    feat: PointsListGenerator added

---
+++ src/main/java/supporters/PointsListGenerator.java
@@ -0,0 +1,23 @@
+package supporters;
+
+import models.Point;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PointsListGenerator {
+    private static Random random;
+
+    public static ArrayList<Point> getArrayList(int size) {
+        ArrayList<Point> pointList = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            double x = random.nextDouble();
+            double y = random.nextDouble();
+
+            Point nextPoint = new Point(x, y);
+            pointList.add(nextPoint);
+        }
+        return pointList;
+    }
+}
