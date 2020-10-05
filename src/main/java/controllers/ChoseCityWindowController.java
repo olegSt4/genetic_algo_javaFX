a9f722f6674d1a649ce41598620543e223e18b18
olegSt4 <stefura2010@ukr.net>
2020-10-06 01:34:25 +0300

    feat(ChoseCityWindowController): points displaying added

--- src/main/java/controllers/ChoseCityWindowController.java
+++ src/main/java/controllers/ChoseCityWindowController.java
@@ -4,22 +4,71 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import models.Point;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ChoseCityWindowController {
     @FXML
     private AnchorPane anchorPane;
 
+    private FXMLLoader dispWinLoader;
+    private String iter;
+    private String mutPercent;
+
     public void start(Parent root, List<Point> points, String iter, String mutPercent, FXMLLoader dispWinLoader) {
+        this.dispWinLoader = dispWinLoader;
+        this.iter = iter;
+        this.mutPercent = mutPercent;
+
         Scene scene = new Scene(root);
+        showPoints(points);
 
         Stage stage = new Stage();
         stage.setTitle("Genetic algorithm");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void showPoints(List<Point> points) {
+        double activeWidth = anchorPane.getPrefWidth();
+        double activeHeight = anchorPane.getPrefHeight();
+
+        int id = 1;
+        for (Point point : points) {
+            Circle circle = new Circle(point.x * activeWidth, point.y * activeHeight, 5);
+            circle.setId(String.valueOf(id));
+
+            circle.setOnMouseEntered(event -> {
+                circle.setStroke(Color.BLUE);
+                circle.setStrokeWidth(4);
+            });
+
+            circle.setOnMouseExited(event -> circle.setStrokeWidth(0));
+
+            circle.setOnMouseClicked(event -> {
+                Stage currentStage = (Stage) circle.getScene().getWindow();
+                currentStage.close();
+
+                try {
+                    Parent root = dispWinLoader.load();
+                    ((DisplayWindowController) dispWinLoader.getController()).start(root, circle.getId(), points, iter, mutPercent);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
+
+            Label idText = new Label(String.valueOf(id++));
+            idText.setLayoutX(point.x * activeWidth + 5);
+            idText.setLayoutY(point.y * activeHeight);
+
+            anchorPane.getChildren().addAll(circle, idText);
+        }
     }
 }
