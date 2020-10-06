4bbc7aec417e21f635e24f261141096ba1c75324
olegSt4 <stefura2010@ukr.net>
2020-10-06 14:15:22 +0300

    feat: displayWindow configured

--- src/main/java/controllers/DisplayWindowController.java
+++ src/main/java/controllers/DisplayWindowController.java
@@ -3,6 +3,7 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -10,13 +11,23 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import models.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DisplayWindowController {
     @FXML
     private AnchorPane anchorPane;
 
-    public void start(Parent root, String startId, List<Point> points, String iter, String mutPercent) {
+    @FXML
+    private Label iter;
+
+    @FXML
+    private Label minDist;
+
+    @FXML
+    private Button startButton;
+
+    public void start(Parent root, String startId, ArrayList<Point> points, String iter, String mutPercent) {
         Scene scene = new Scene(root);
         showPoints(points, startId);
 
@@ -26,20 +37,20 @@ public class DisplayWindowController {
         stage.show();
     }
 
-    private void showPoints(List<Point> points, String startId) {
+    private void showPoints(ArrayList<Point> points, String startId) {
         double activeWidth = anchorPane.getPrefWidth();
         double activeHeight = anchorPane.getPrefHeight();
 
-        int id = 1;
-        for (Point point : points) {
-            Circle circle = new Circle(point.x * activeWidth, point.y * activeHeight, 5);
-            if (id == Integer.parseInt(startId)) {
+        for (int index = 0; index < points.size(); index++) {
+            Point nextPoint = points.get(index);
+            Circle circle = new Circle(nextPoint.x * activeWidth, nextPoint.y * activeHeight, 5);
+            if (index + 1 == Integer.parseInt(startId)) {
                 circle.setRadius(7);
                 circle.setFill(Color.RED);
             }
-            Label idText = new Label(String.valueOf(id++));
-            idText.setLayoutX(point.x * activeWidth + 5);
-            idText.setLayoutY(point.y * activeHeight);
+            Label idText = new Label(String.valueOf(index + 1));
+            idText.setLayoutX(nextPoint.x * activeWidth + 5);
+            idText.setLayoutY(nextPoint.y * activeHeight);
 
             anchorPane.getChildren().addAll(circle, idText);
         }
