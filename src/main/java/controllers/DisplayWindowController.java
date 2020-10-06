2f56abfd05056c13a40c01038155a75779f5897c
olegSt4 <stefura2010@ukr.net>
2020-10-06 14:27:28 +0300

    feat(DisplayWindowController): preparation for displaying results

--- src/main/java/controllers/DisplayWindowController.java
+++ src/main/java/controllers/DisplayWindowController.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -25,11 +27,18 @@ public class DisplayWindowController {
     private Label minDist;
 
     @FXML
+    private Pane pane;
+
+    @FXML
     private Button startButton;
+
+    private Task<Void> drawRoutes;
 
     public void start(Parent root, String startId, ArrayList<Point> points, String iter, String mutPercent) {
         Scene scene = new Scene(root);
+
         showPoints(points, startId);
+        setStartButtonOnAction(startId, points, iter, mutPercent);
 
         Stage stage = new Stage();
         stage.setTitle("Genetic Algorithm");
@@ -54,5 +63,35 @@ public class DisplayWindowController {
 
             anchorPane.getChildren().addAll(circle, idText);
         }
+    }
+
+    private void setStartButtonOnAction(String startId, ArrayList<Point> points, String iter, String mutPercent) {
+        startButton.setOnAction(event -> {
+            pane.getChildren().remove(startButton);
+
+            drawRoutes = createTask(startId, points, iter, mutPercent);
+            Thread thread = new Thread(drawRoutes);
+            thread.setDaemon(true);
+            thread.start();
+        });
+    }
+
+    private Task<Void> createTask(String startId, ArrayList<Point> points, String iter, String mutPercent) {
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                try {
+                    displayResultsOfGeneticAlgo(startId, points, iter, mutPercent);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+
+            private void displayResultsOfGeneticAlgo(String startId, ArrayList<Point> points, String iter, String mutPercent) {
+                // Get geneticAlgo results and display it
+            }
+
+        };
     }
 }
