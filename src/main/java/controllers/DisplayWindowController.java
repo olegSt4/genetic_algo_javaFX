8e888091235b3f23f8d5b05cefbc49da680f74fa
olegSt4 <stefura2010@ukr.net>
2020-10-07 14:52:10 +0300

    feat!(DisplayWindowController): routes displaying implements

--- src/main/java/controllers/DisplayWindowController.java
+++ src/main/java/controllers/DisplayWindowController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -9,22 +10,29 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
 import models.Point;
+import models.Route;
+import supporters.GeneticAlgorithm;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class DisplayWindowController {
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
-    private Label iter;
+    private Label iterLabel;
 
     @FXML
-    private Label minDist;
+    private Label minDistLabel;
 
     @FXML
     private Pane pane;
@@ -34,11 +42,11 @@ public class DisplayWindowController {
 
     private Task<Void> drawRoutes;
 
-    public void start(Parent root, String startId, ArrayList<Point> points, String iter, String mutPercent) {
+    public void start(Parent root, String startId, ArrayList<Point> points, String iterNum, String mutPercent) {
         Scene scene = new Scene(root);
 
         showPoints(points, startId);
-        setStartButtonOnAction(startId, points, iter, mutPercent);
+        setStartButtonOnAction(startId, points, iterNum, mutPercent);
 
         Stage stage = new Stage();
         stage.setTitle("Genetic Algorithm");
@@ -64,33 +72,105 @@ public class DisplayWindowController {
         }
     }
 
-    private void setStartButtonOnAction(String startId, ArrayList<Point> points, String iter, String mutPercent) {
+    private void setStartButtonOnAction(String startId, ArrayList<Point> points, String iterNum, String mutPercent) {
         startButton.setOnAction(event -> {
             pane.getChildren().remove(startButton);
 
-            drawRoutes = createTask(startId, points, iter, mutPercent);
+            drawRoutes = createTask(startId, points, iterNum, mutPercent);
             Thread thread = new Thread(drawRoutes);
             thread.setDaemon(true);
             thread.start();
         });
     }
 
-    private Task<Void> createTask(String startId, ArrayList<Point> points, String iter, String mutPercent) {
+    private Task<Void> createTask(String startId, ArrayList<Point> points, String iterNum, String mutPercent) {
         return new Task<Void>() {
             @Override
             protected Void call() throws Exception {
                 try {
-                    displayResultsOfGeneticAlgo(startId, points, iter, mutPercent);
+                    displayResultsOfGeneticAlgo(startId, points, iterNum, mutPercent);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
                 return null;
             }
 
-            private void displayResultsOfGeneticAlgo(String startId, ArrayList<Point> points, String iter, String mutPercent) {
-                // Get geneticAlgo results and display it
-            }
+            private void displayResultsOfGeneticAlgo(String startId, ArrayList<Point> points, String iterNum, String mutPercent) {
+                GeneticAlgorithm genAlg = new GeneticAlgorithm(points, Integer.parseInt(startId), Integer.parseInt(mutPercent),
+                        anchorPane.getPrefWidth(), anchorPane.getPrefHeight());
 
+                int iterations = Integer.parseInt(iterNum);
+                for (int i = 0; i < 3; i++) {
+                    boolean isLast = i == 3 - 1 ? true : false;
+
+                    Route nextRoute = genAlg.nextPopulationBestRoute();
+                    String minDistance = new DecimalFormat("#.##").format(nextRoute.length);
+                    String currentIter = String.valueOf(i + 1);
+                    Platform.runLater(() -> {
+                        drawOneRoute(nextRoute, isLast);
+                        iterLabel.setText(currentIter);
+                        minDistLabel.setText(minDistance);
+                    });
+                }
+            }
         };
+    }
+
+    private void drawOneRoute(Route route, boolean isSpecial) {
+        double activeWidth = anchorPane.getPrefWidth();
+        double activeHeight = anchorPane.getPrefHeight();
+
+        Path path = new Path();
+
+        MoveTo startPos = new MoveTo();
+        startPos.setX(route.citySequence.get(0).x * activeWidth);
+        startPos.setY(route.citySequence.get(0).y * activeHeight);
+
+        path.getElements().add(startPos);
+        for (int i = 1; i < route.citySequence.size(); i++) {
+            LineTo nextPoint = new LineTo();
+            nextPoint.setX(route.citySequence.get(i).x * activeWidth);
+            nextPoint.setY(route.citySequence.get(i).y * activeHeight);
+            path.getElements().add(nextPoint);
+        }
+
+        if (!isSpecial) {
+            path.setStroke(getRandomColor());
+            path.setStrokeWidth(1);
+
+            path.setOnMouseEntered(event -> {
+                path.setStrokeWidth(4);
+            });
+
+            path.setOnMouseExited(event -> {
+                path.setStrokeWidth(1);
+            });
+        } else {
+            path.setStroke(Color.RED);
+            path.setStrokeWidth(5);
+        }
+
+
+        anchorPane.getChildren().add(path);
+    }
+
+    private Color getRandomColor() {
+        Random rand = new Random();
+        int choice = rand.nextInt(10);
+
+        switch (choice) {
+            case 0 : return Color.BLUE;
+            case 1 : return Color.YELLOW;
+            case 2 : return Color.GREEN;
+            case 3 : return Color.GRAY;
+            case 4 : return Color.BLACK;
+            case 5 : return Color.ORANGE;
+            case 6 : return Color.VIOLET;
+            case 7 : return Color.DARKGREEN;
+            case 8 : return Color.PINK;
+            case 9 : return Color.BROWN;
+
+            default: return Color.BLACK;
+        }
     }
 }
