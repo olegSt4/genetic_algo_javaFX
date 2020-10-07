1d1c754c578235685dfe677539832c948b356e2d
olegSt4 <stefura2010@ukr.net>
2020-10-07 19:30:46 +0300

    feat: ChildPare structure is added (used is krossover)

---
+++ src/main/java/models/ChildPare.java
@@ -0,0 +1,11 @@
+package models;
+
+public class ChildPare {
+    public final Route firstChild;
+    public final Route secondChild;
+
+    public ChildPare(Route firstChild, Route secondChild) {
+        this.firstChild = firstChild;
+        this.secondChild = secondChild;
+    }
+}
